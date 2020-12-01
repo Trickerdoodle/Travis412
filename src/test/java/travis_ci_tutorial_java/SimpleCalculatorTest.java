668856d4c293fec418a597ade303002d052a0d79
Melissa Trick <melissatrick@gmail.com>
2020-12-01 00:46:23 -0500

    subtraction

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -11,4 +11,8 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.add(1, 1), 2);
 	}
+	public void testSubt() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.subt(5, 3), 2);
+	}
 }
